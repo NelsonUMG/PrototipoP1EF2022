8b61808414f5bc87969c384ed16cdec0c3cac516
Nelson Pineda <npinedac3@miumg.edu.gt>
2022-06-16 10:40:20 -0600

    creación de clase menu general

---
+++ include/MenuGeneral.h
@@ -0,0 +1,16 @@
+#ifndef MENUGENERAL_H
+#define MENUGENERAL_H
+
+
+class MenuGeneral
+{
+    public:
+        MenuGeneral();
+        virtual ~MenuGeneral();
+
+    protected:
+
+    private:
+};
+
+#endif // MENUGENERAL_H
